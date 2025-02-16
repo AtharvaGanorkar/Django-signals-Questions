bcb32f5f8841a716cb9254f0679a4ef22c1a3db5
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-02-16 06:19:18 +0000

    Signal Q2

--- Django Signals Questions/Question2.py
+++ Django Signals Questions/Question2.py
@@ -3,17 +3,16 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
-class Model(models.Model):
+class AGModel(models.Model):
     name = models.CharField(max_length=100)
 
-@receiver(post_save, sender=Model)
+@receiver(post_save, sender=AGModel)
 def my_signal_handler(sender, instance, **kwargs):
     print(f"Signal handler running in this thread: {threading.get_ident()}")
 
 def test_signal():
     print(f"Caller running in this thread: {threading.get_ident()}")
-    Model.objects.create(name="Test")
+    AGModel.objects.create(name="Test")
 
 
 test_signal()
