ba9126a35cafc7989f584b75dbd6f1effe31c429
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-02-16 06:17:02 +0000

    Signal Q1

--- Django Signals Questions/Question1.py
+++ Django Signals Questions/Question1.py
@@ -1,23 +1,24 @@
+# Import necessary modules
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 import time
 
-class AgModel(models.Model):
+
+class AGModel(models.Model):
     name = models.CharField(max_length=100)
 
 
-@receiver(post_save, sender=AgModel)
-def Ag_signal(sender, instance, **kwargs):
+@receiver(post_save, sender=AGModel)
+def my_signal_handler(sender, instance, **kwargs):
     print("Signal handler started")
-    time.sleep(3)  
+    time.sleep(8)  
     print("Signal handler finished")
 
-
 def test_signal():
-    print("Creating AgModel instance")
-    AgModel.objects.create(name="Test")
-    print("AgModel instance created")
+    print("Creating AGModel instance...")
+    AGModel.objects.create(name="Test")
+    print("AGModel instance created")
 
 
 test_signal()
