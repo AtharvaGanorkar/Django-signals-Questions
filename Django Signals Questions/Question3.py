9501ce7056eda506ea18516962e34f836c89c3a6
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-02-16 06:23:06 +0000

    Signal Q3

--- Django Signals Questions/Question3.py
+++ Django Signals Questions/Question3.py
@@ -6,11 +6,11 @@ from django.dispatch import receiver
 class MyModel(models.Model):
     name = models.CharField(max_length=100)
 
-
 @receiver(post_save, sender=MyModel)
 def my_signal_handler(sender, instance, **kwargs):
     print("Signal handler: Creating another MyModel instance...")
     MyModel.objects.create(name="Signal-created instance")
+
 
 def test_signal():
     try:
@@ -19,12 +19,9 @@ def test_signal():
             MyModel.objects.create(name="Caller-created instance")
             raise Exception("Forcing a rollback")
     except Exception as e:
-        print(f"Exception caught: {e}")
-
+       print(f"Exception caught: {e}")
 
 test_signal()
 
-# Check the database state
-print("Final database state:")
 for obj in MyModel.objects.all():
     print(f"ID: {obj.id}, Name: {obj.name}")
